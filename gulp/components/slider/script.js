cefa6eb84fa8cffb755dd8068482f327af566a25
maks_chaliy <mrgoodchaliy@yandex.ru>
2022-04-06 21:59:02 +0300

    add slider

---
+++ gulp/components/slider/script.js
@@ -0,0 +1,28 @@
+console.log(`slider`);
+
+// const $trainersSlider = document.querySelector('.trainers__slider');
+
+// let trainersSlider = new Swiper($trainersSlider, {
+
+// 	// pagination: {
+// 	// 	el: '.intro-pag',
+// 	// 	type: 'bullets',
+// 	// 	clickable: true,
+// 	// }
+// 	slidesPerView: 2,
+// 	spaceBetween: 10,
+// 	breakpoints: {
+// 		769: {
+// 			slidesPerView: 3,
+// 			spaceBetween: 40,
+// 		}
+// 	},
+// 	scrollbar: {
+// 		el: '.trainers__scroll',
+// 		dragable: true,
+// 	},
+// 		navigation: {
+// 		nextEl: '.trainers__slider-btn--next',
+// 		prevEl: '.trainers__slider-btn--prev',
+// 	},
+// })
