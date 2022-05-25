a3216e9a6dfa2e4ffb9513da90462edf6e98a6a8
maks_chaliy <mrgoodchaliy@yandex.ru>
2022-05-25 21:12:04 +0300

    add files

--- src/js/app.js
+++ src/js/app.js
@@ -1,18 +1,8 @@
-//=============/=============/=============
-//========== SETTINGS ==========
+
 
 import * as flsFunctions from "./settings/webp.js";
 flsFunctions.isWebp();
 
-//=============/=============/=============
-//========== MODULES ==========
+//=============/подключение модулей js/=============
 //import '../js/modules/anchors.js';
 // import '../js/modules/burger.js';
-
-
-//=============/=============/=============
-//========== FILES ==========
-// import './files/anchors.js';
-// import './files/burger.js';
-// import './files/modals.js';
-// import './files/slider.js';
